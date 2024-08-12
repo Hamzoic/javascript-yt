bf18297c863d9c04b8d2486e53b66549521ec8e8
Hamza Nasir Lodi <152505079+Hamzoic@users.noreply.github.com>
2024-08-12 12:52:52 +0000

    devcontainer config done

---
+++ begin_01/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 3872378
+let accountEmail = "Hamza@gmail.com"
+var accountPassword = "HelloBoi"
+accountCity = "Karachi"
+
+//accountId=34
+console.log("accountId");
+console.table([accountId, accountEmail, accountPassword, accountCity]);
